f311830e9b8268d8a079f5569b59a21c4bab0056
Genny Ocampo <gegex08@aol.com>
2024-01-18 11:53:41 -0600

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+// Fig. 2.1: Welcome1.java
+// Text-printing program.
+public class JavaApplication1
+{
+// main method begins execution of Java application
+//Outputs: Selections and Change
+// Inputs: Money, Selection
+// Assumptions: Full of Change and selections
+// Constraint: 1's and 5's, number or selections
+// Equations: Change == Money - 1
+// Design
+// 1. Insert Money
+// 2. Make Selection
+// 3. Output Change
+// 4. Calculate Change
+// 5. Output Change
+public static void main( String[] args )
+{
+System.out.println( "Soda Machine" );
+double cars1 = 4;
+    System.out.println(cars1*2);
+} // end method main
+} // end class Welcome1
