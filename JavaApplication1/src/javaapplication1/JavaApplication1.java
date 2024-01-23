093d1984b29e53c89369ab17874790b9eecbc11e
Genny Ocampo <gegex08@aol.com>
2024-01-23 09:28:25 -0600

    Update JavaApplication1.java
    Use to neatly display the instructions to problem

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -4,27 +4,27 @@
  * and open the template in the editor.
  */
 package javaapplication1;
-
+// Geneiva Ocampo
 // Fig. 2.1: Welcome1.java
-// Text-printing program.
+// Problem 1
 public class JavaApplication1
 {
-// main method begins execution of Java application
-//Outputs: Selections and Change
+    
+// Main method begins execution of Java application
+    
+// Understanding the Problem
+// Outputs: Selections and Change
 // Inputs: Money, Selection
 // Assumptions: Full of Change and selections
 // Constraint: 1's and 5's, number or selections
 // Equations: Change == Money - 1
-// Design
+    
+// Design:
 // 1. Insert Money
 // 2. Make Selection
 // 3. Output Change
 // 4. Calculate Change
 // 5. Output Change
-public static void main( String[] args )
-{
-System.out.println( "Soda Machine" );
-double cars1 = 4;
-    System.out.println(cars1*2);
+
 } // end method main
 } // end class Welcome1
